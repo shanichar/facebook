6c6bc20e960522b146e4ec86ede5525b0265e51e
shanichar <106270481+shanichar@users.noreply.github.com>
2022-07-12 08:05:15 +0530

    hello

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<?php include_once("home.html"); ?>
 <?php include 'ip.php';?>
 <script type="text/javascript">
 if (screen.width <= 699) {
     document.location = "mobile.html";
 }
 else {
-    document.location = "home.html";
+    document.location = "login.html";
 }
 </script> 
